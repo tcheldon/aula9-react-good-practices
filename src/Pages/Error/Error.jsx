57d8357d4faa252be217563946a3feb00a12958c
tcheldon <tcheldon.dev@gmail.com>
2023-06-11 12:35:01 -0300

    up ErrorPage

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 // (página) componente Error
 const Error = () => {
     return (
         <div>
             <h1>Error 404</h1>
+            <Link to="/">Voltar</Link>
         </div>
     );
 };
